2326f0e14ea9c8cb9618b2bc18e314091969657c
Paul Wood <904paul@gmail.com>
2019-08-10 12:37:54 -0400

    Added some device parameters & renamed project to "PowerSenseModule"

--- PowerSenseModule.X/nbproject/Makefile-impl.mk
+++ PowerSenseModule.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=PowerSensorModule.X
+PROJECTNAME=PowerSenseModule.X
 
 # Active Configuration
 DEFAULTCONF=default
--- PowerSenseModule.X/nbproject/Package-default.bash
+++ PowerSenseModule.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/PowerSensorModule.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=PowerSensorModule.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=powersensormodule.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/PowerSenseModule.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=PowerSenseModule.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=powersensemodule.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/powersensormodule.x/bin
+makeDirectory ${TMPDIR}/powersensemodule.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/powersensormodule.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/powersensemodule.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/powersensormodule.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/powersensemodule.x.tar *
 checkReturnCode
 
 # Cleanup
